a8428be591be799817cc19b99a0a5f8fddd06782
PompousPro <pompouspro@PompousPros-MacBook-Pro.local>
2020-05-25 14:41:43 -0700

    add progress time and scroll bar functionality in js

--- script.js
+++ script.js
@@ -25,12 +25,26 @@ const updatePlayIcon = () => {
 // Update Progress & Timestamp
 
 const updateProgress = () => {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get Minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  // Get Seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 };
 
 // Set Video Time To Progress
 const setVideoProgress = () => {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 };
 
 // Stop Video
